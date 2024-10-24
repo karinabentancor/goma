d8e3cb4325f0d8e735c76f883489757d3adcd5d8
karinabentancor <karinabentancorlenzi@gmail.com>
2024-10-24 20:17:57 -0300

    --

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.getElementById('toggleAside').addEventListener('click', function() {
 document.getElementById('closeAside').addEventListener('click', function() {
     document.getElementById('asideMenu').classList.remove('active');
 });
+
+
+const texts = [
+    "Texto aleatorio 1",
+    "Texto aleatorio 2",
+    "Texto aleatorio 3",
+    "Texto aleatorio 4",
+    "Texto aleatorio 5",
+];
+
+const carouselItems = document.querySelectorAll('.carousel-item h5');
+
+carouselItems.forEach((item, index) => {
+    item.textContent = texts[Math.floor(Math.random() * texts.length)];
+});
